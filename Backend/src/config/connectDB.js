b426c37a72a5c589886bcf2daf01300e5c361ea5
baonguyen179 <baohp17@gmail.com>
2026-03-10 20:20:21 +0700

    build: update docker-compose to use Docker Hub image

--- Backend/src/config/connectDB.js
+++ Backend/src/config/connectDB.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    dialect: 'mysql',
+    dialect: process.env.DB_DIALECT,
     port: process.env.DB_PORT,
 
     pool: {
